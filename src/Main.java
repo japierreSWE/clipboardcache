b6877dee6f40cee976bf68009bae8c855985e334
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2019-05-12 15:00:45 -0400

    Setup view switches to cache view and inits model after inputting size

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -8,8 +10,11 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import com.japierre.ClipboardCache.entity.Model;
 
 public class Main extends Application {
+	
+	Model model;
 	
 	//set up scene and layout.
 	VBox setupRoot = new VBox(8);
@@ -18,12 +23,12 @@ public class Main extends Application {
 	//make view for setup
 	Scene setupView = new Scene(setupRoot, 400, 100);
 	Label label = new Label("Number of elements in cache:");
+	Label errorMessage = new Label("Please input a number.");
 	TextField field = new TextField();
 	HBox labelTextfieldContainer = new HBox(8);
 	
 	//make view for cache
 	Scene cacheView = new Scene(cacheRoot, 300, 600);
-	
 	
 	public void start(Stage primaryStage) throws Exception {
 		
@@ -35,7 +40,34 @@ public class Main extends Application {
 		
 		setupRoot.getChildren().add(labelTextfieldContainer);
 		setupRoot.getChildren().add(button);
+		setupRoot.getChildren().add(errorMessage);
 		setupRoot.setAlignment(Pos.CENTER);
+		
+		//shouldn't see error message yet.
+		errorMessage.setVisible(false);
+		
+		//give the button an event handler
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			
+			//when the button is pressed, switch to the other view
+			public void handle(ActionEvent arg0) {
+				
+				String input = field.getText();
+				
+				try {
+					
+					int size = Integer.valueOf(input);
+					model = new Model(size);
+					primaryStage.setScene(cacheView);
+					primaryStage.show();
+					
+				} catch(NumberFormatException e) {
+					errorMessage.setVisible(true);
+				}
+				
+			}
+			
+		});
 		
 		//prepare stage's title and scene. then show it.
 		primaryStage.setTitle("Clipboard Cache");
--- src/com/japierre/ClipboardCache/entity/Model.java
+++ src/com/japierre/ClipboardCache/entity/Model.java
@@ -5,12 +5,16 @@ public class Model {
 	int cacheSize; //the size of the cache
 	
 	public Model() {
-		cacheSize = 0;
+		
 	}
 	
-	//sets the size of the cache.
-	public void setSize(int size) {
+	//initialize a model with a certain cache size
+	public Model(int size) {
 		cacheSize = size;
+	}
+	
+	public int getSize() {
+		return cacheSize;
 	}
 	
 }
